e5165347ad88a32d0ba730e7cdbd3adc44d1c219
Brendan Meyer <brenmeyer2245@gmail.com>
2018-07-23 16:03:14 -0500

    Created Category Model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,8 @@
 const User = require('./user')
+const Category = require('./category')
+// const Product = require('./product')
+
+//Category.hasMany(Product, through: 'Product-Category')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -14,5 +18,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Category
 }
