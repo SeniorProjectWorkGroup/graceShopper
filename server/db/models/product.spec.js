55d189a2dbbec971a68068c4ea3df1da452ee799
Jehoshuah K <musicalbard@gmail.com>
2018-07-23 17:12:32 -0500

    added test spec file for product model

---
+++ server/db/models/product.spec.js
@@ -0,0 +1,83 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai')
+const db = require('../index')
+const Product = db.model('product')
+
+describe('Product model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+      let newItem
+
+      beforeEach(async () => {
+        newItem = await Product.create({
+          name: 'Shiny Thingamabob',
+          numInStock: 3,
+          price: 5.99,
+          description: "This thing is so dang shiny. You need special sunglasses just to see it. Don't be the only person on your block without one!",
+          imageUrls: ["https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjN0ODGmbbcAhVOUt8KHRP_BQIQjRx6BAgBEAU&url=https%3A%2F%2Fmedium.com%2Fthe-bridge%2Fthat-shiny-thing-you-like-so-much-went-through-a-process-thats-why-it-works-7552638e7232&psig=AOvVaw2XE_Vz7Yjktl9spikuqH0h&ust=1532468852972335", "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiihpbgmbbcAhWM3oMKHfa3CnwQjRx6BAgBEAU&url=https%3A%2F%2Fmotionarray.com%2Fstock-motion-graphics%2Fneon-disco-ball-31645&psig=AOvVaw320MZNXT3wsPqx54A25p1g&ust=1532468920008956"]
+        })
+      });
+
+  describe('Product model', () => {
+    it('has a name', () => {
+      expect(newItem.name).to.be.equal('Shiny Thingamabob');
+    });
+
+    it('name cannot be null', () => {
+      const item = Product.build();
+      return item.validate().then(
+        // if validation succeeds (bad), this function will fire
+        () => {
+          throw new Error('Validation should fail without a name');
+        },
+        // if validation fails (good), this function will fire
+        result => {
+          expect(result).to.be.an.instanceOf(Error);
+        }
+      );
+    });
+
+    it('has a number in stock', () => {
+      expect(newItem.numInStock).to.be.equal(3);
+    });
+
+    it('has a price', () => {
+      expect(newItem.price).to.be.equal(5.99);
+    });
+
+    it('price cannot be less than 0', () => {
+      const item = Product.build({ name: 'Lexie', price: -1 });
+      return item.validate().then(
+        // if validation succeeds (bad), this function will fire
+        () => {
+          throw new Error('Validation should fail when age is less than 0');
+        },
+        // if validation fails (good), this function will fire
+        result => {
+          expect(result).to.be.an.instanceOf(Error);
+        }
+      );
+    });
+
+    it('has a description', () => {
+      const item = Product.build({ name: 'Stuff', description: "it's very nice" });
+      expect(item.description).to.be.equal("its's very nice");
+    });
+
+    it('description cannot be null', () => {
+      const item = Product.build({name: "thing"});
+      return item.validate().then(
+        // if validation succeeds (bad), this function will fire
+        () => {
+          throw new Error('Validation should fail without a name');
+        },
+        // if validation fails (good), this function will fire
+        result => {
+          expect(result).to.be.an.instanceOf(Error);
+        }
+      );
+    });
+  })
+})
