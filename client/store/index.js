d80a8ab7a7460421e6dd9cf7ca972ff375a1f6d7
Jehoshuah K <musicalbard@gmail.com>
2018-07-26 17:33:02 -0500

    created cart/line item componenets. hooked up reducers but need work on line item reducer and cart reducer

--- client/store/index.js
+++ client/store/index.js
@@ -7,13 +7,17 @@ import productList from './products/productsList'
 import currentProduct from './products/singleProduct'
 import {categories} from './category'
 import {displayedProducts} from './products/displayedProducts'
+import cartReducer from './cartReducer'
+import lineItemReducer from './lineItemReducer'
 
 const reducer = combineReducers({
   user,
   productList,
   categories,
   currentProduct,
-  displayedProducts
+  displayedProducts,
+  cartReducer,
+  lineItemReducer
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
