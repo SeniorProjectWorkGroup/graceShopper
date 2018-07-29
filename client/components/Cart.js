ec5701903e2aceb15ace4d485fe4222005c16fde
Brendan Meyer <brenmeyer2245@gmail.com>
2018-07-29 07:29:57 -0500

    Added Edit Quantity Buttons

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,25 +3,17 @@ import {connect} from 'react-redux'
 import CartItem from './CartItem'
 import {fetchCart} from '../store/cartReducer'
 
-let i = 1
-
-function Cart(props) {
-  return (
-    <div>
-      Welcome to your cart!
-      <ul>
-        console.log(props)
-        {props.cartItems.map(item => <CartItem item={item} key={item.id} />)}
-      </ul>
-    </div>
-  )
-}
-
 class CartLoader extends Component {
   constructor(props) {
     super(props)
     this.state = {
       requested: false
+    }
+  }
+  componentDidMount() {
+    if (this.props.user.cartId && !this.state.requested) {
+      this.props.loadCart(this.props.user.cartId)
+      this.setState({requested: true})
     }
   }
 
@@ -34,12 +26,12 @@ class CartLoader extends Component {
   }
 
   render() {
-    // return <Cart {...this.props} />
     if (!this.props.user.cartId) {
       return <h1> Loading Cart</h1>
     } else {
+      /*Calculate Vitual Variables for Total and Tax*/
       const sumTotal = this.props.cartItems.reduce((sum, item) => {
-        sum = sum + item.product.price
+        sum = sum + item.product.price * item.quantity
         return sum
       }, 0)
       const tax = Math.round(0.0875 * sumTotal * 100) / 100
@@ -55,6 +47,7 @@ class CartLoader extends Component {
                   quantity={item.quantity}
                   item={item.product}
                   key={item.id}
+                  itemId={item.id}
                 />
               )
             })}
