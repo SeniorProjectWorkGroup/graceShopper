e7f181660ea8a4b76a52f29b45562d79ab4784d8
brandonjyee <yee.brandon.j@gmail.com>
2018-07-30 18:53:49 -0500

    working on creating reviews

--- client/components/ReviewSection.js
+++ client/components/ReviewSection.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import StarRating from './StarRating'
+import {postReview} from '../store/reviews'
 
 class ReviewSection extends React.Component {
+
   toDateString = dateStr => {
     const options = {year: 'numeric', month: 'long', day: 'numeric'}
     return new Date(dateStr).toLocaleDateString('en-US', options)
@@ -10,6 +12,17 @@ class ReviewSection extends React.Component {
   submitReview = event => {
     console.log('submitReview. target:', event.target)
     event.preventDefault()
+
+    const formData = new FormData(event.target);
+    console.log('FormData:', formData, 'reviewText:', formData.get('reviewText'))
+    const reviewText = formData.get('reviewText')
+    // let campusToSubmit = { id: this.props.match.params.id };
+    // ['name', 'address', 'imageUrl', 'description'].forEach(key => {
+    //   campusToSubmit[key] = formData.get(key);
+    // });
+    // console.log('In EditCampus. campusToSubmit:', campusToSubmit);
+    // this.props.putCampus(campusToSubmit);
+    this.props.postReview(productId, review)
   }
 
   render() {
@@ -22,10 +35,18 @@ class ReviewSection extends React.Component {
         <div>
           Leave a review
           <form onSubmit={this.submitReview}>
+            {/* <label htmlFor="reviewTitle">Title</label> */}
+            <input id="reviewTitle" name="title" type="text" value="Title" />
+            <br/>
+            {/* <label htmlFor="reviewRating">Rating</label> */}
+            <input id="reviewRating" name="rating" type="" value="Rating (0 - 5)"/>
+            <br/>
             <textarea
               className="review-input-box"
+              name="reviewText"
               placeholder="Write your review here. What did you like most? What did you like the least?"
             />
+            <br/>
             <button type="submit">Write Review</button>
           </form>
         </div>
@@ -35,7 +56,7 @@ class ReviewSection extends React.Component {
               <div key={review.id}>
                 <div>
                   <img
-                    src={review.user.imageUrl}
+                    src={review.user.imageUrl || '/icons/default-user.svg'}
                     className="user-review-icon"
                   />{' '}
                   {review.user.name}
