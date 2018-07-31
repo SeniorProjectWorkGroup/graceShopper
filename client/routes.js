26827bd10354defd2f0aad4e9690bca9ad9ebb79
Jehoshuah K <musicalbard@gmail.com>
2018-07-31 12:01:44 -0500

    fixed syntax error

--- client/routes.js
+++ client/routes.js
@@ -108,7 +108,7 @@ const mapState = state => {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
     isLoggedIn: !!state.user.id,
-    admin: state.user.role === 'ADMIN'
+    admin: state.user.role === 'ADMIN',
     userRole: state.user.role
   }
 }
